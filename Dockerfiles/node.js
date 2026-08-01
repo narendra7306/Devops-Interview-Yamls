11a2ae3e001917cc82db81fa1b2a8f77d99192c0
narendra7306 <narendrareddy0a3@gmail.com>
2026-08-01 11:48:04 +0000

    added node.js dockerfile

---
+++ Dockerfiles/node.js
@@ -0,0 +1,21 @@
+# Base Image
+FROM node:20-alpine
+
+# Set working directory
+WORKDIR /app
+
+# Copy package files
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy application source code
+COPY . .
+
+# Expose application port
+EXPOSE 3000
+
+# Start the application
+CMD ["npm", "start"]
+
